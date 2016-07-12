bdaefaa63d07ba8ca6d349e56182dc18fd60f77f
Albert Kaaman <albert@kaaman.nu>
2016-07-12 15:40:11 +0200

    Fixed problem when port has no / unknown service

--- src/Nmap/Nmap.php
+++ src/Nmap/Nmap.php
@@ -247,9 +247,9 @@ class Nmap
                 (string) $port->attributes()->protocol,
                 (string) $port->state->attributes()->state,
                 new Service(
-                    (string) $port->service->attributes()->name,
-                    (string) $port->service->attributes()->product,
-                    (string) $port->service->attributes()->version
+                    $port->service ? (string) $port->service->attributes()->name : 'N/A',
+                    $port->service ? (string) $port->service->attributes()->product : 'N/A',
+                    $port->service ? (string) $port->service->attributes()->version : 'N/A'
                 )
             );
         }
